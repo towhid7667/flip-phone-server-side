b6b5cb0ca4f046ba387c416d3c4d144ce43fb833
towhid7667 <towhidulhq@gmail.com>
2022-11-28 14:59:28 +0600

    best deal server fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const port = process.env.PORT || 5000
 require("dotenv").config();   
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -8,16 +9,16 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.blm4ehx.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.blm4ehx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try{
-        const besdealsDatabase = client.db("FlipPhone").collection("bestDeals");
+        const bestdealsDatabase = client.db("FlipPhone").collection("bestDeals");
 
         app.get('/bestDeals', async(req, res) => {
             const query = {};
-            const result = await besdealsDatabase.find(query);
+            const result = await bestdealsDatabase.find(query).toArray();
             res.send(result);
         })
 
