6865d32faf6ce0df84a9ac873201146b9e431406
towhid7667 <towhidulhq@gmail.com>
2022-11-28 14:07:10 +0600

    best deal database opened

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+const uri = "mongodb+srv://<username>:<password>@cluster0.blm4ehx.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const besdealsDatabase = client.db("FlipPhone").collection("bestDeals");
+
+        app.get('/bestDeals', async(req, res) => {
+            const query = {};
+            const result = await besdealsDatabase.find(query);
+            res.send(result);
+        })
+
+
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', async(req, res) =>{
     res.send('server is booming')
