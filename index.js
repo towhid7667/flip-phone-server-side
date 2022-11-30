424b6ff505879f95111044ffe8f8c11b06bd853c
towhid7667 <towhidulhq@gmail.com>
2022-11-30 21:21:10 +0600

    Buyer admin seller added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 require("dotenv").config();   
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -12,12 +13,69 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.blm4ehx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.send(401).send('unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try{
         const bestdealsDatabase = client.db("FlipPhone").collection("bestDeals");
         const androidDatabase = client.db("FlipPhone").collection("android");
         const appleDatabase = client.db("FlipPhone").collection("AppleProducts");
         const gamingphonesDatabase = client.db("FlipPhone").collection("gamingphones");
+        const usersDatabase = client.db("FlipPhone").collection("users");
+
+
+
+        const verifyAdmin = async (req, res, next) =>{
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersDatabase.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+        const verifyBuyer = async (req, res, next) =>{
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersDatabase.findOne(query);
+
+            if (user?.role !== 'Buyer') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+        const verifySeller = async (req, res, next) =>{
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersDatabase.findOne(query);
+
+            if (user?.role !== 'Seller') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next();
+        }
+
+
+
+
+
 
         app.get('/bestDeals', async(req, res) => {
             const query = {};
@@ -39,9 +97,47 @@ async function run() {
             const result = await gamingphonesDatabase.find(query).toArray();
             res.send(result);
         })
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await usersDatabase.insertOne(user);
+            res.send(result);
+        })
 
 
+        app.get('/jwt', async(req, res) => {
+            const email = req.query.email;
+            const query = {email : email};
+            const user = await usersDatabase.findOne(query);
 
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn : '3h'})
+                return res.send({accessToken : token})
+            }
+            res.status(403).send({accessToken : ''})
+
+        })
+
+        app.get('/users/admin/:email',verifyJWT, verifyAdmin, async(req, res) => {
+            const email = req.params.email;
+            const query = {email};
+            const user = await usersDatabase.findOne(query)
+            res.send({isAdmin: user?.role === 'admin'})
+
+        })
+        app.get('/users/buyer/:email',verifyJWT, verifyBuyer, async(req, res) => {
+            const email = req.params.email;
+            const query = {email};
+            const user = await usersDatabase.findOne(query)
+            res.send({isBuyer: user?.role === 'Buyer'})
+
+        })
+        app.get('/users/seller/:email',verifyJWT, verifySeller, async(req, res) => {
+            const email = req.params.email;
+            const query = {email};
+            const user = await usersDatabase.findOne(query)
+            res.send({isSeller: user?.role === 'Seller'})
+
+        })
 
     }
     finally{
