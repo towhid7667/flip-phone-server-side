0d5ecb26f397b848dc798464e0a456e3edb3e0a5
towhid7667 <towhidulhq@gmail.com>
2022-12-02 11:14:22 +0600

    product detail page added

--- index.js
+++ index.js
@@ -164,6 +164,12 @@ async function run() {
             const result = await productDatabase.find(query).toArray();
             res.send(result);
         })
+        app.get('/allproducts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await productDatabase.find(query).toArray();
+            res.send(result);
+        })
 
 
 
